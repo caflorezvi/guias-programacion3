5c12b5d2b91ed6a4b260e55edf598bd4f8976b60
Carlos Andrés Florez <caflorez@uniquindio.edu.co>
2025-08-28 17:42:56 -0500

    Corrección de fórmulas de recursividad

---
+++ ejemplos/division.exs
@@ -0,0 +1,24 @@
+defmodule Matematicas do
+
+  def main do
+    dividir(10, 0) |> imprimir() |> IO.puts()
+  end
+
+  def dividir(a,b) do
+    cond do
+      b == 0 -> {:error, "No se puede dividir por cero"}
+      true -> {:ok, a / b}
+    end
+  end
+
+  def imprimir({resp, valor}) do
+    case resp do
+      :ok -> "El resultado es: #{valor}"
+      :error -> "Error: #{valor}"
+    end
+  end
+
+end
+
+
+Matematicas.main()
