0eb233bf289283c672546180a262578b159d207e
Carlos Andrés Florez <caflorez@uniquindio.edu.co>
2026-02-20 10:34:19 -0500

    Código de las notas mejorado

--- ejemplos/basicos/promedios.exs
+++ ejemplos/basicos/promedios.exs
@@ -1,33 +1,39 @@
 defmodule Notas do
+
   def main do
-    Util.leer("Escriba 1 para calcular la nota definitiva del curso 1, o 2 para el curso 2: ", :integer)
-    |> procesar_curso()
-    |> Util.imprimir()
+    "Ingrese 1 para calcular la nota del curso 1 (promedio) o 2 para la nota del curso 2 (porcentajes): "
+    |> Util.leer(:integer)
+    |> procesar()
+    |> Util.imprimir_mensaje()
   end
 
-  defp procesar_curso(curso) when curso not in 1..2 do
-    "El valor ingresado es incorrecto"
+  defp procesar(curso) when curso == 1 or curso == 2, do: calcular_y_generar_mensaje(curso)
+  defp procesar(_), do: "Error, curso incorrecto"
+
+  defp calcular_y_generar_mensaje(curso) do
+    notas = leer_notas()
+    nota_final = calcular_nota(curso, notas)
+
+    aprobado?(nota_final)
+    |> generar_mensaje(nota_final)
   end
 
-  defp procesar_curso(curso) do
-    notas = pedir_datos()
-    calcular_promedio(curso, notas)
-    |> generar_mensaje()
+  defp leer_notas do
+    n1 = Util.leer("Ingrese la nota 1: ", :float)
+    n2 = Util.leer("Ingrese la nota 2: ", :float)
+    n3 = Util.leer("Ingrese la nota 3: ", :float)
+    n4 = Util.leer("Ingrese la nota 4: ", :float)
+
+    {n1, n2, n3, n4}
   end
 
-  defp pedir_datos do
-    nota1 = Util.leer("Ingrese la nota 1: ", :float)
-    nota2 = Util.leer("Ingrese la nota 2: ", :float)
-    nota3 = Util.leer("Ingrese la nota 3: ", :float)
-    nota4 = Util.leer("Ingrese la nota 4: ", :float)
-    {nota1, nota2, nota3, nota4}
-  end
+  defp calcular_nota(1, {n1, n2, n3, n4}), do: (n1 + n2 + n3 + n4) / 4
+  defp calcular_nota(2, {n1, n2, n3, n4}), do: n1 * 0.15 + n2 * 0.30 + n3 * 0.35 + n4 * 0.20
 
-  defp calcular_promedio(1, {n1, n2, n3, n4}), do: (n1 + n2 + n3 + n4) / 4
-  defp calcular_promedio(2, {n1, n2, n3, n4}), do: n1 * 0.15 + n2 * 0.3 + n3 * 0.35 + n4 * 0.2
+  defp aprobado?(nota), do: nota >= 3.0
 
-  defp generar_mensaje(promedio) when promedio >= 3, do: "Su nota definitiva es: #{promedio} por lo tanto ha aprobado"
-  defp generar_mensaje(promedio),do: "Su nota definitiva es: #{promedio} por lo tanto ha reprobado"
+  defp generar_mensaje(true, nota), do: "El estudiante aprobó el curso con una nota final de #{Float.round(nota, 2)}."
+  defp generar_mensaje(false, nota), do: "El estudiante no aprobó el curso. La nota final fue #{Float.round(nota, 2)}."
 
 end
 
